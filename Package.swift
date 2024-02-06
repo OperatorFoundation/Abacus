39b45de6a16c500977f594bdb7805f587d67b85e
bragelbytes <joebragel@gmail.com>
2024-02-06 12:48:17 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main")
+        .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
