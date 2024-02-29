b61b59ea424abdfc3fc04e962f7b37fb0da0ab7c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-02-29 11:40:29 -0600

    Updated Abacus iOS version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Abacus",
-    platforms: [.macOS(.v12)],
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
