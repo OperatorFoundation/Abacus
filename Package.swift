aa38da305b4c87e435791fdc9d91b81bceef1bbd
bragelbytes <joebragel@gmail.com>
2024-11-03 16:23:42 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Gardener", from: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Gardener", from: "0.1.3"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1")
     ],
     targets: [
